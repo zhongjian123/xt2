952552545b400a085af2303839fe714f64100a14
zhongjian123 <1483204905@qq.com>
2018-03-09 19:18:27 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,21 +3,15 @@ window.onload=function(){
 			var timer;
 			timer=setInterval(hao,2500)
 			function hao(){
-				var ht=new Array(4);
-				ht[0]="images/slider-5.jpg";
-				ht[1]="images/slider-1.jpg";
-				ht[2]="images/slider-2.jpg";
-				ht[3]="images/slider-3.jpg";
-				ht[4]="images/slider-4.jpg";
-				ht[5]="images/slider-5.jpg";
-				ht[6]="images/slider-1.jpg";
+				var ht=new Array(5);
+				ht[0]="images/slider-1.jpg";
+				ht[1]="images/slider-2.jpg";
+				ht[2]="images/slider-3.jpg";
+				ht[3]="images/slider-4.jpg";
+				ht[4]="images/slider-5.jpg";
 		    var x=document.getElementById("photos");
 		    x.setAttribute("src",ht[i]);
-		    if(i==6){i=0;}
+		    if(i==4){i=0;}
 		    else{i++;}
 			}
-		}
-function a1(){
-	var x=document.getElementById("photos");
-	x.setAttribute("src",ht[0]);
-}
+	}
